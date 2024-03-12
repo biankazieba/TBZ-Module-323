7a7c3d80baa0c623aeb347d5d4f378781079f313
bianka.zieba <bianka.zieba@edu.tbz.ch>
2024-03-12 11:46:41 +0100

    [FEATURE] - Add FunctionAsValue Task

---
+++ FunctionAsValue/src/main/scala/Main.scala
@@ -0,0 +1,16 @@
+
+object Main {
+
+
+  def main(args: Array[String]): Unit = {
+    MapTasks.run();
+    FilterTasks.run();
+    FilterMapTasks.run();
+    FoldLeftTasks.run();
+    FlatMapTasks.run();
+    ForComprehensionsTasks.run();
+  }
+
+
+
+}
